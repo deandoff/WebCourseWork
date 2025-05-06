bf9247a5855a8a5abf60214125bc969f2894714e
deandoff <deandoff@gmail.com>
2025-05-06 11:55:59 +0300

    db connection

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
-const app = express()
+const models = require('./models/models')
+const cors = require('cors')
 
 const port = process.env.PORT || 3000
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Hello'})
+})
 
 const start = async () => {
     try {
--- server/models/models.js
+++ server/models/models.js
@@ -3,7 +3,72 @@ const {DataTypes} = require('sequelize')
 
 const User = sequelize.define('user', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    email: {type: DataTypes.STRING, unique: true},
+    username: {type: DataTypes.STRING, unique: true},
     password: {type: DataTypes.STRING},
-    role: {type: DataTypes.STRING, defaultValue: "USER"}
+    fullname: {type: DataTypes.STRING},
+    email: {type: DataTypes.STRING, unique: true},
+    role: {type: DataTypes.STRING, defaultValue: "USER"},
+    createdate: {type: DataTypes.DATE, defaultValue: new Date()},
 })
+
+const Group = sequelize.define('group', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true},
+    course: {type: DataTypes.INTEGER}
+})
+
+const Classroom = sequelize.define('classroom', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    number: {type: DataTypes.INTEGER},
+    building: {type: DataTypes.INTEGER},
+})
+
+const Subject = sequelize.define('subject', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING},
+    type: {type: DataTypes.STRING},
+})
+
+const StudentGroup = sequelize.define('student_group', {
+    student_id: {type: DataTypes.INTEGER},
+    group_id: {type: DataTypes.INTEGER},
+})
+
+const Schedule = sequelize.define('schedule', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    subject_id: {type: DataTypes.INTEGER},
+    teacher_id: {type: DataTypes.INTEGER},
+    group_id: {type: DataTypes.INTEGER},
+    classroom_id: {type: DataTypes.INTEGER},
+    day_of_week: {type: DataTypes.INTEGER},
+    start_time: {type: DataTypes.TIME},
+    end_time: {type: DataTypes.TIME},
+})
+
+Schedule.hasMany(User)
+User.belongsTo(Schedule)
+
+Schedule.hasMany(Group)
+Group.belongsTo(Schedule)
+
+Schedule.hasMany(Classroom)
+Classroom.belongsTo(Schedule)
+
+Schedule.hasMany(Subject)
+Subject.belongsTo(Schedule)
+
+StudentGroup.hasMany(User)
+User.belongsTo(StudentGroup)
+
+StudentGroup.hasMany(Group)
+Group.belongsTo(StudentGroup)
+
+module.exports = {
+    User,
+    Group,
+    Classroom,
+    Subject,
+    StudentGroup,
+    Schedule
+}
+
